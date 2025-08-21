969746d7f3cc855c8b58b2956d0e3447aaf86399
nathanconceicao <nathanconceidev@gmail.com>
2025-08-21 20:57:21 -0300

    Botão Igual
    Foi adicionado switch,  variaveis, messagembox, para tratamento de erro e usado pela primeira vez a biblioteca do .Net o DataTable. Computer para fazer expressão matematicas usando string.

--- CalculadoraForm/Form1.Designer.cs
+++ CalculadoraForm/Form1.Designer.cs
@@ -276,6 +276,7 @@
             this.btnLimpar.TabIndex = 15;
             this.btnLimpar.Text = "C";
             this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // txbTela
             // 
--- CalculadoraForm/Form1.cs
+++ CalculadoraForm/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -22,7 +23,53 @@ namespace CalculadoraForm
 
         private void btnIgual_Click(object sender, EventArgs e)
         {
-            // Implementar depois...
+            string mat = txbTela.Text;
+
+            // Verificar se o campo está vazio
+            if (mat == "")
+            {
+                MessageBox.Show("Erro, o campo está vazio", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Pega o último caractere da expressão digitada
+            char ultimoOperador = mat[mat.Length - 1];
+
+            // Escolha caso para identificar o operador
+            switch (ultimoOperador)
+            {
+                case '-':
+                case '+':
+                case '/':
+                case '*':
+                    // Caso ele nenhum operador
+                    MessageBox.Show("Erro,verique o operador.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                // Uma biblioteca importada o .Net para fazer expressoes matematicas com strings e armazenar na variavel resultado 
+                var resultado = new System.Data.DataTable().Compute(mat, null).ToString();
+            
+                // Mostra o resultado
+                txbTela.Text = resultado.ToString();
+                // Erro ao calcular por 0 (zero)
+                if (resultado == "∞")
+                {
+                    txbTela.Text = "";
+                    MessageBox.Show("Erro ao calcular expressão, por dividir o número por zero ", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    
+                }
+                // "Ele levanta a bandeirinha novamente" para iniciar novamente o programa e digitar o proximo numero
+                operadorClicado = true;
+            }
+            catch
+            {
+                // 
+                MessageBox.Show("Erro", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txbTela.Text = "";
+            }
         }
 
         private void numero_Click(object sender, EventArgs e)
@@ -50,6 +97,14 @@ namespace CalculadoraForm
                 // Mudar o operadorClicado para true (levantar bandeirinha)):
                 operadorClicado = true;
             } 
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            // Limpar a tela
+            txbTela.Text = "";
+            // Voltar o operador clicado para true:
+            operadorClicado = true;
         }
     }
 }
